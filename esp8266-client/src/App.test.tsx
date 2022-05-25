f4ef9d422cb8bbd147d451705605093a219d23d8
Pavel Shibanov <paulshibanov+github.com@gmail.com>
2022-05-25 13:49:40 +0200

    feat: add esp client

--- esp8266-client/src/App.test.tsx
+++ esp8266-client/src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", async () => {
+  const { getByText } = render(<App />);
+  await waitFor(() => {
+    expect(getByText("ESP 8266 State")).toBeInTheDocument();
+  });
+  const linkElement = getByText(/esp 8266 state/i);
   expect(linkElement).toBeInTheDocument();
 });
--- esp8266-client/src/App.tsx
+++ esp8266-client/src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useEffect } from "react";
+import { Layout, Menu, Breadcrumb, Switch, Divider } from "antd";
+import "./App.css";
+const { Content, Header, Footer } = Layout;
 
-function App() {
+const baseUrl = process.env.REACT_APP_ESP_HOST;
+
+type IsOnResponse = {
+  data: { isOn: boolean };
+};
+type PinsResponse = {
+  data: { pins: number[] };
+};
+type PinState = { [key: number]: boolean };
+
+const pinColorClasses: { [key: number]: string } = {
+  4: "switch-green",
+  5: "switch-red",
+};
+
+const App: FC = () => {
+  const [pins, setPins] = React.useState<number[]>([]);
+  const [ledsIsOn, setLedIsOn] = React.useState<PinState>({});
+  useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    const syncLedState = async () => {
+      const result = await fetch(`${baseUrl}/pins`, { signal });
+      const {
+        data: { pins },
+      }: PinsResponse = await result.json();
+      setPins(pins);
+      const newState: PinState = {};
+      for (const pin of pins) {
+        const result = await fetch(
+          `${baseUrl}/is-on?${new URLSearchParams({ pin: pin.toString() })}`,
+          { signal }
+        );
+        const {
+          data: { isOn },
+        }: IsOnResponse = await result.json();
+        newState[pin] = isOn;
+      }
+
+      setLedIsOn(newState);
+    };
+    syncLedState();
+    return () => controller.abort();
+  }, []);
+  const onChange = async (pin: number) => {
+    const response = await fetch(
+      `${baseUrl}/toggle?${new URLSearchParams({ pin: pin.toString() })}`,
+      { method: "POST" }
+    );
+    const {
+      data: { isOn },
+    }: IsOnResponse = await response.json();
+    setLedIsOn({ ...ledsIsOn, [pin]: isOn });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          items={[{ key: "home", label: "ESP 8266 State" }]}
+        />
+      </Header>
+      <Content style={{ padding: "100px 50px" }}>
+        <div style={{ textAlign: "center" }} className="site-layout-content">
+          {pins.map((pin) => (
+            <div key={pin} className={`${pinColorClasses[pin]}`}>
+              <h4>Pin: {pin}</h4>
+              <Switch checked={ledsIsOn[pin]} onChange={() => onChange(pin)} />
+              <Divider />
+            </div>
+          ))}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>©2022</Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
