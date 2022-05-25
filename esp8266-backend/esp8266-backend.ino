a62b6a4ffb0e59f3ed67f67c012802d8fdf2d47b
Pavel Shibanov <paulshibanov+github.com@gmail.com>
2022-05-25 14:01:06 +0200

    refactor: esp8266 backend

--- esp8266-backend/esp8266-backend.ino
+++ esp8266-backend/esp8266-backend.ino
@@ -7,17 +7,27 @@
 #include <ESPAsyncTCP.h>
 #endif
 #include <ESPAsyncWebServer.h>
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+#include "LittleFS.h"
+
+int ledPins[] = {4, 5};
 
 AsyncWebServer server(80);
 
 const char *ssid = "ssid";
 const char *password = "password";
 
-const char *PARAM_MESSAGE = "message";
+const char *PARAM_PIN = "pin";
 
 void notFound(AsyncWebServerRequest *request)
 {
-    request->send(404, "text/plain", "Not found");
+    AsyncResponseStream *response = request->beginResponseStream("application/json");
+    DynamicJsonDocument json(1024);
+    json["error"] = "Not found";
+    serializeJson(json, *response);
+    response->setCode(404);
+    request->send(response);
 }
 
 void setup()
@@ -34,31 +44,60 @@ void setup()
 
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
+
+    if (!LittleFS.begin())
+    {
+        Serial.println("Failed to mount FS");
+    }
+    for (int p : ledPins)
+    {
+        pinMode(p, OUTPUT);
+        digitalWrite(p, LOW);
+    }
+    server.serveStatic("/", LittleFS, "/")
+        .setDefaultFile("index.html");
+
+    server.on("/pins", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
+        AsyncResponseStream *response = request->beginResponseStream("application/json");
+        DynamicJsonDocument json(1024);
+        JsonArray pins = json["data"].createNestedArray("pins");
+        for (int p : ledPins)
+        {
+            pins.add(p);
+            Serial.println(p);
+        };
+        serializeJson(json, *response);
+        request->send(response); });
 
     server.on("/is-on", HTTP_GET, [](AsyncWebServerRequest *request)
               {
-                if (digitalRead(LED_BUILTIN) == HIGH)
-                {
-                    request->send(200, "text/plain", "true");
-                }
-                else
-                {
-                    request->send(200, "text/plain", "false");
-                } });
+        int pin = ledPins[0];
+        if( request->hasParam(PARAM_PIN) )
+        {
+            pin = request->getParam(PARAM_PIN)->value().toInt();
+        } 
+        bool isOn = digitalRead(pin) == HIGH;
+        AsyncResponseStream *response = request->beginResponseStream("application/json");
+        DynamicJsonDocument json(1024);
+        json["data"]["isOn"] = isOn;
+        serializeJson(json, *response);
+        request->send(response); });
 
     server.on("/toggle", HTTP_POST, [](AsyncWebServerRequest *request)
               {
-                  
-                    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-        String message;
-        if (request->hasParam(PARAM_MESSAGE, true)) {
-            message = request->getParam(PARAM_MESSAGE, true)->value();
-        } else {
-            message = "No message sent";
-        }
-        request->send(200, "text/plain", "Hello, POST: " + message); });
+        int pin = ledPins[0];
+        if( request->hasParam(PARAM_PIN) )
+        {
+            pin = request->getParam(PARAM_PIN)->value().toInt();
+        } 
+        int isOn = digitalRead(pin);
+        digitalWrite(pin, !isOn);
+        AsyncResponseStream *response = request->beginResponseStream("application/json");
+        DynamicJsonDocument json(1024);
+        json["data"]["isOn"] = (bool)!isOn;
+        serializeJson(json, *response);
+        request->send(response); });
 
     server.onNotFound(notFound);
 
